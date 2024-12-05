d898b98887ef7bcf25ef7979c59044ffc017cb07
BhavyaG-1288 <gbhavya2017@gmail.com>
2024-12-05 23:10:29 +0530

    feat/login

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function signup(str){
 }
 signup("Veekshitha")
 signup("Bhanu")
+
+function login(str,password){
+    let filetredData = userNames.filter((el,i)=> el ==str);
+    if (filetredData.length ==0) {
+        console.log("User Not Found....Please Signup");
+    }else {
+        if(password == "Emp@123") {
+            console.log("Login Sucessfull.......");
+        }else {
+            console.log("Wrong Password......");
+        }
+    }
+}
+login("Bhavya","Emp@123");
